8628e14f910c51e93dbcc0dac094b939a9dad805
Shahzod <shaydulovshahzod454@gmail.com>
2026-08-20 14:12:09 +0500

    mobilViewning tezligini oshirish uchun kichik o'zgartirishlar kiritildi

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,20 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-import uz from './locales/uz.json';
-import en from './locales/en.json';
-import ru from './locales/ru.json';
+import resourcesToBackend from 'i18next-resources-to-backend';
 
 i18n
   .use(LanguageDetector)
+  .use(resourcesToBackend((language) => import(`./locales/${language}.json`)))
   .use(initReactI18next)
   .init({
-    resources: {
-      uz: { translation: uz },
-      en: { translation: en },
-      ru: { translation: ru },
-    },
     fallbackLng: 'uz',
     supportedLngs: ['uz', 'en', 'ru'],
     interpolation: {
@@ -25,6 +18,12 @@ i18n
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
       lookupLocalStorage: 'makon360_language',
+    },
+    react: {
+      // Tarjima hali yuklanayotganda ilova "to'xtab qolmasligi" (Suspense
+      // xatosiga uchramasligi) uchun - o'rniga kalit nomi bir zumga
+      // ko'rinib, millisekundlar ichida to'g'ri matnga almashadi
+      useSuspense: false,
     },
   });
 
